f6664940ea36c0fb530ccc4a2f1b1a296b33cd22
Trương Nguyễn Quang Thái <87488896+TNQT14@users.noreply.github.com>
2023-12-30 15:35:18 +0700

    Connect dtb

--- public/src/server.js
+++ public/src/server.js
@@ -4,7 +4,31 @@ const routes = express.Router();
 const mongoose = require('mongoose');
 const app = express()
 
-mongoose.connect('mongodb://gunner:123423423@172.16.0.124:27102/hcm_backen_test')
+mongoose.connect('')
+
+const Schema = mongoose.Schema;
+const ObjectID = Schema.ObjectId;
+
+const hcmtestCreate = new Schema({
+    userName: String,
+    password: String,
+}, {
+    collection: 'Account'
+});
+
+const AccountModel = mongoose.model('account', hcmtestCreate);
+
+AccountModel.find({})
+    .then(function (data) {
+        console.log('data', data);
+    })
+    .catch(error => {
+        console.log('err', err);
+    })
+
+data => {
+
+}
 
 app.listen(3012, () => {
     console.log("Server is running hcm_backen_test2")
