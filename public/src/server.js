da36df1690329200520a11b90c3504f833086ece
Trương Nguyễn Quang Thái <87488896+TNQT14@users.noreply.github.com>
2024-01-02 16:05:20 +0700

    Read successfull

--- public/src/server.js
+++ public/src/server.js
@@ -1,9 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const bodyParse = require('body-parser');
-const bodyParser = require('body-parser');
-const routes = express.Router();
-require('dotenv').config()
-console.log(process.env)
+var MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+console.log('bước1')
+var mongo = new MongoClient(process.env.connectionStr);
+console.log('bước2')
+console.log(process.env.connectionStr)
+
+mongo.connect().then(() => {
+    // Kết nối thành công, giờ đây bạn có thể truy cập database và collection
+    const db = mongo.db(); // Lấy đối tượng database
+    const collection = db.collection(process.env.USERTABLE); // Lấy đối tượng collection
+
+    // Lấy bản ghi đầu tiên trong collection
+    collection.find().toArray().then((document) => {
+        console.log(document);
+    });
+});
+console.log("Connect thành công")
 
