12645c50b0da63122b9dc3caa34054b78c625ed4
Trương Nguyễn Quang Thái <87488896+TNQT14@users.noreply.github.com>
2024-01-02 16:47:39 +0700

    Read data

--- public/src/server.js
+++ public/src/server.js
@@ -5,15 +5,25 @@ var mongo = new MongoClient(process.env.connectionStr);
 console.log('bước2')
 console.log(process.env.connectionStr)
 
-mongo.connect().then(() => {
+mongo.connect().then(async () => {
     // Kết nối thành công, giờ đây bạn có thể truy cập database và collection
     const db = mongo.db(); // Lấy đối tượng database
     const collection = db.collection(process.env.USERTABLE); // Lấy đối tượng collection
 
-    // Lấy bản ghi đầu tiên trong collection
-    collection.find().toArray().then((document) => {
+    // Lấy tất cả recode trong collection
+    console.log("Tất cả bản ghi");
+    await collection.find().toArray().then((document) => {
         console.log(document);
+    });
+
+    console.log("Lấy bản ghi đầu tiên");
+    await collection.findOne().then((document) => {
+        console.log(document)
+    });
+
+    console.log("Tìm kiếm");
+    await collection.find({ name: "Le Duyen Thien" }).toArray().then((documents) => {
+        console.log(documents)
     });
 });
 console.log("Connect thành công")
-
