3e4ff9b9958ea55f7c0a2a560fd312b9d6a0d47b
Trương Nguyễn Quang Thái <87488896+TNQT14@users.noreply.github.com>
2024-01-02 23:22:02 +0700

    Tìm kiếm một user rồi delete

--- public/src/server.js
+++ public/src/server.js
@@ -11,19 +11,41 @@ mongo.connect().then(async () => {
     const collection = db.collection(process.env.USERTABLE); // Lấy đối tượng collection
 
     // Lấy tất cả recode trong collection
-    console.log("Tất cả bản ghi");
+    console.log("===========Tất cả bản ghi==========");
     await collection.find().toArray().then((document) => {
         console.log(document);
     });
 
-    console.log("Lấy bản ghi đầu tiên");
+    console.log("==============Lấy bản ghi đầu tiên===========");
     await collection.findOne().then((document) => {
         console.log(document)
     });
 
-    console.log("Tìm kiếm");
+    console.log("=============Tìm kiếm====================");
     await collection.find({ name: "Le Duyen Thien" }).toArray().then((documents) => {
         console.log(documents)
+    });
+
+    // console.log("=============Ghi dữ liệu====================");
+    // const document = JSON.parse({
+    //     "userId": 934252569,
+    //     "name": "Quang Thai",
+    // });
+    // collection.insertOne(document).then(() => {
+    //     // Xử lý dữ liệu
+    // });
+
+    console.log("=============Xóa dữ liệu====================");
+    const documents = await collection.find({ name: "Le Duyen Thien" }).toArray();
+
+    if (documents.length > 0) {
+        collection.deleteOne(documents[0]).then(() => {
+            // Xử lý dữ liệu
+        });
+    }
+
+    await collection.find().toArray().then((document) => {
+        console.log(document);
     });
 });
 console.log("Connect thành công")
